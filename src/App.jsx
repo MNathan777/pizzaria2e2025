0841216553e0c1cbc3658aa6a3a39dccb5ee6190
Miguel Nathan <miguelnathan606@gmail.com>
2025-04-04 14:13:09 +0000

    nova alteração

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,58 @@
 import { useState } from "react"
-
+ 
 function App(){
-
-
-  const [nome, setNome] = useState('Cr7')
-  const [idade, setIdade] = useState('18')
-  const [time, setTime] = useState('Corinthians FC')
+ 
+  const [nome, setNome] = useState('Ronaldo')
+  const [idade, setIdade] = useState(18)
+  const [time, setTime] = useState('Santos FC')
   const [pi] = useState(3.14)
-
+ 
   //var idade = 18;
-  //let time = 'Corinthians FC'
+  //let time = 'Santos FC';
   //const pi = 3.14;
-
-  const Formulario = (props)=> {
-    const [valor, serValor] = useState()
-
-
+ 
+  const Formulario = (props) => {
+    const [valor, setValor] = useState()
+   
     return (
       <div>
-        <input 
+        <input
           className = "nome"
           placeholder= {props.sombra}
           onChange={(e)=>{setValor(e.target.value)}}
           type="text" />
-
-        <button 
+ 
+        <button
           className="botao"
           onClick={()=>{
-            if(valor.length > 0)
+            if(valor.length > 0){
               alert(valor);
-          
-            else
-            alert(props.nome + 'idade é' + props.idade 
-            
-          )}}      
+            }
+            else{
+              alert(props.nome + ' idade é ' + props.idade);
+            }
+            }}      
         >
           CLIQUE AQUI
         </button>
       </div>
     )
   }
-
-
+ 
+ 
   return (
     <div>
       <h3>Pizzaria 2E</h3>
-
-      <Formulario nome="Ricardo" 
-       idade={45}
-       sombra="Digite seu nome..." />
-      <Formulario nome="Maria" sombra="Digite sua idade..." />
-      <Formulario  nome="Tomas" sombra="Digite sua time..." />
-      <Formulario  nome="Antonieta" sombra="Digite sua pi..." />
-
+ 
+      <Formulario nome="Ricardo"
+          idade={45}
+          sombra="Digite nome..." />
+      <Formulario nome="Maria" sombra="Digite sua idade..."/>
+      <Formulario nome="Tomas" sombra="Digite seu time..." />
+      <Formulario nome="Antonieta" sombra="valor de pi" />
+ 
     </div>
   )
 }
-
+ 
 export default App
