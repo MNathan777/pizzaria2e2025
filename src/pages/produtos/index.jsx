eeba45dbf249225cf5f40f868be5053aeb0211ff
Miguel Nathan <miguelnathan606@gmail.com>
2025-09-15 11:30:14 +0000

    consumo

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,13 +1,36 @@
+import {axios} from 'axios'
+import { useEffect, useState } from 'react';
+
+
 const Produtos = () => {
+
+    app.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
+    .then((response)=>{
+        alert(JSON.stringify(response))
+    })
+    
+    // Consumir os produtos do endpoint (rota)
+    useEffect = (() => {
+
+
+    }, []);
+    // Objetos que possui lista de pizzas
+    const pizzas = [
+        "Pizza de mussarela",
+        "Pizza de Calabresa",
+        "Pizza Baiana",
+        "Pizza Portuguesa",
+        "Pizza de Atum", "Pizza de seis queijos"
+    ];
+    // Mapeamento das pizzas da lista (iteração)
+    const listaPizzas = pizzas.map(pizza => <li>{pizza}</li>);
 
     return(
     <>
-     <h3>Projeto Pizzaria</h3>
+     <h3>Listagem de Produtos</h3>
         <ul>
-          <li>Pizza de mussarela</li>
-        <li>Pizza de Calabresa</li>
-      <li>Pizza Baiana</li>
-      <li>Pizza Portuguesa</li>
+        {listaPizzas}
+
         </ul>
         </>
     )
