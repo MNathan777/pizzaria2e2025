290cb9cae3e49e4f26470537d889dfabcc5192c6
Miguel Nathan <miguelnathan606@gmail.com>
2025-09-15 10:56:19 +0000

    listagem de produtos

---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,16 @@
+const Produtos = () => {
+
+    return(
+    <>
+     <h3>Projeto Pizzaria</h3>
+        <ul>
+          <li>Pizza de mussarela</li>
+        <li>Pizza de Calabresa</li>
+      <li>Pizza Baiana</li>
+      <li>Pizza Portuguesa</li>
+        </ul>
+        </>
+    )
+}
+
+export default Produtos
