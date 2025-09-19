9649176a51e36ad0528a3ac7e73bc75d5a9068e6
Miguel Nathan <miguelnathan606@gmail.com>
2025-09-19 14:06:55 +0000

    mudança

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,12 @@ import Produtos from "./pages/produtos"
 const App = () => {
 
   return (
-  <div>
-    <h1>Projeto Pizzaria</h1>
+    <div>
+        <h1>Projeto Pizzaria</h1>
+        <Produtos />
+          
+              </div>
+           )
+     }
 
-    <h3>Listagem de Produtos</h3>
-    <ul>
-      <li>Pizza de Mussarela</li>
-      <li>Pizza de Calabresa</li>
-      <li>Pizza Baiana</li>
-      <li>Pizza Portuguesa</li>
-    </ul>
-    </div>
-    )
-}
-
-export default App
+ export default App
